aeea1c2d65baab867b06a3ebd6f9470624907f8e
MinChul-Son <smc5236@gmail.com>
2022-02-09 16:22:29 +0900

    ✨ feat: Spring AOP 적용
    - controller 패키지에 적용
    - 걸린 시간과 호출한 target을 로깅

---
+++ src/main/java/com/fastcampus/practice/aspect/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.fastcampus.practice.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Pointcut("within(com.fastcampus.practice.controller..*)")
+    public void onRequest() {
+    }
+
+    @Around("onRequest()")
+    public Object doLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+        final long start = System.currentTimeMillis();
+
+        try {
+            return joinPoint.proceed(joinPoint.getArgs());
+        } finally {
+            final long end = System.currentTimeMillis();
+            log.info("target: {}", joinPoint.getSignature());
+            log.warn("걸린시간: {}ms", end - start);
+        }
+    }
+}용
